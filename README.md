f872030445743c3fae2528c60613015b954f8f0b
Karanpreet Singh <karanpreetsingh2@rrc.ca>
2023-09-12 12:36:22 -0500

    Initial commit: Documented the project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Module 2 Demonstration
+## Description
+- Introduction to python
+- Documentation
+- Variables
+- Printing
+
+## Author
+Karanpreet Singh
---
+++ module02.py
@@ -0,0 +1,13 @@
+"""
+Description: Module 2 Demonstration.
+Author: Karanpreet Singh
+Date: September 12, 2023
+Usage: Run the application by clicking the run button in the IDE(Integrated Development Environment)
+"""
+
+# This is an inline comment.
+
+"""
+This is a multiline comment.
+It can be span over multiple lines of code.
+"""
