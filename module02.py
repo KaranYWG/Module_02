9e8593c48d40d60832ff9139d10f3ad758c8a77a
Karanpreet Singh <karanpreetsingh2@rrc.ca>
2023-09-12 13:36:28 -0500

    experimented with printing and variables

--- module02.py
+++ module02.py
@@ -11,3 +11,28 @@ Usage: Run the application by clicking the run button in the IDE(Integrated Deve
 This is a multiline comment.
 It can be span over multiple lines of code.
 """
+
+absolute_value = abs(-12)
+print("Absolute value:", absolute_value)
+print("Absolute value:"+ str(absolute_value))
+print("Absolute value:", abs(-12))
+
+print("I am", "20", "years old")
+print("apples", "mangoes", "bananas", sep="**")
+
+# Variables
+
+name = "John"
+age = 20
+pi = 3.15445
+is_a_student = True
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_a_student))
+
+print(f"My name is {name} and I am {age} years old." )
+print(f"The value of pi to 2 decimal places is {pi:.2f}")
+print(f"Age zero-filled is {age:05}")
+print(f"Hello: {name:> }")
+print(f"${salary:,.2f}")
